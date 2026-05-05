4e949a9d4321f4230d4d092a56b18a950e4a9332
Ivnn24 <your-email@example.com>
2026-05-05 13:09:28 +0800

    Update COOP header to unsafe-none for Google popup

--- next.config.ts
+++ next.config.ts
@@ -24,7 +24,7 @@ const nextConfig: NextConfig = {
           },
           {
             key: "Cross-Origin-Opener-Policy",
-            value: "same-origin-allow-popups",
+            value: "unsafe-none",
           },
         ],
       },
